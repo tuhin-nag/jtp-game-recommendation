736bc00c5f3595935d93663461ccf40b67715a8e
Soulninja4 <getarceus@gmail.com>
2024-04-01 19:15:45 +0530

    refactored NavBar

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -9,15 +9,28 @@ const GameCard = ({ game }) => {
 
   const handleClickAddToLibrary = async () => {
     setIsAdded(!isAdded);
-    try {
-      const response = await axios.get(`http://localhost:5000/add_to_library/${game.name}`);
-      if (response.data.data) {
-        console.log('Game added to library:', response.data);
-      } else {
-        console.error('Failed to add game to library:', response.data);
+    if (!isAdded) {
+      try {
+        const response = await axios.get(`http://localhost:5000/add_to_library/${game.name}`);
+        if (response.data.data) {
+          console.log('Game added to library:', response.data);
+        } else {
+          console.error('Failed to add game to library:', response.data);
+        }
+      } catch (error) {
+        console.error('Error calling API:', error);
       }
-    } catch (error) {
-      console.error('Error calling API:', error);
+    } else {
+      try {
+        const response = await axios.get(`http://localhost:5000/remove_from_library/${game.name}`);
+        if (response.data.data) {
+          console.log('Game removed from library:', response.data);
+        } else {
+          console.error('Failed to remove game from library:', response.data);
+        }
+      } catch (error) {
+        console.error('Error calling API:', error);
+      }
     }
   };
   return (
@@ -39,8 +52,28 @@ const GameCard = ({ game }) => {
         }}></div>
       <div className="game-title">
         <span>{game.name}</span>
-        {/* <button className="save-game" onClick={handleClickAddToLibrary}></button> */}
-        <svg className='save-game' xmlns="http://www.w3.org/2000/svg" height="30" width="30" viewBox="0 0 384 512" onClick={handleClickAddToLibrary}><path fill="#ffffff" d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z" /></svg>
+        {!isAdded ? (
+          <svg xmlns="http://www.w3.org/2000/svg"
+            className="save-game"
+            height="30"
+            width="30"
+            viewBox="0 0 384 512"
+            onClick={handleClickAddToLibrary}><path fill="#ffffff" d="M0 48C0 21.5 21.5 0 48 0l0 48V441.4l130.1-92.9c8.3-6 19.6-6 27.9 0L336 441.4V48H48V0H336c26.5 0 48 21.5 48 48V488c0 9-5 17.2-13 21.3s-17.6 3.4-24.9-1.8L192 397.5 37.9 507.5c-7.3 5.2-16.9 5.9-24.9 1.8S0 497 0 488V48z" /></svg>
+        ) : (
+          <svg
+            className="save-game"
+            xmlns="http://www.w3.org/2000/svg"
+            height="30"
+            width="30"
+            viewBox="0 0 384 512"
+            onClick={handleClickAddToLibrary}
+          >
+            <path
+              fill="#ffffff"
+              d="M0 48V487.7C0 501.1 10.9 512 24.3 512c5 0 9.9-1.5 14-4.4L192 400 345.7 507.6c4.1 2.9 9 4.4 14 4.4c13.4 0 24.3-10.9 24.3-24.3V48c0-26.5-21.5-48-48-48H48C21.5 0 0 21.5 0 48z"
+            />
+          </svg>
+        )}
       </div>
     </div>
   );
