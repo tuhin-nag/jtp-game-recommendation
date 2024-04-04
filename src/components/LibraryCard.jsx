cfab4b8cce5ded04e2b1fdbc352c15e3063b436a
Soulninja4 <getarceus@gmail.com>
2024-04-04 16:40:20 +0530

    created library page

---
+++ src/components/LibraryCard.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import axios from 'axios'
+import './librarycard.css'
+
+const LibraryCard = ({ game, fetchDataAgain }) => {
+  const handleRemoveFromLibrary = async () => {
+    try {
+      const response = await axios.get(`http://localhost:5000/remove_from_library/${game.name}`)
+      if (response.data.data) {
+        console.log('Game removed from library:', response.data)
+      } else {
+        console.error('Failed to remove game from library:', response.data)
+      }
+    } catch (error) {
+      console.error('Error calling API:', error)
+    }
+    fetchDataAgain()
+  }
+  return (
+    <div className='grid-item'>
+      <img src={game.header_image} alt={game.name} />
+      <div className='library-card-footer'>
+        <div className='library-game-title'>{game.name}</div>
+        <div className="library-button" onClick={handleRemoveFromLibrary}>
+          <span className="library-button-text">Remove From Library</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LibraryCard
--- src/pages/Library.jsx
+++ src/pages/Library.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import NavBar from '../components/NavBar'
+import axios from 'axios'
 import './library.css'
+import LibraryCard from '../components/LibraryCard'
 
 function Library() {
+  const [games, setGames] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:5000/get_library')
+      .then(response => {
+        setGames(response.data.data)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error)
+      })
+  }, [])
+
+  const fetchDataAgain = () => {
+    axios.get('http://localhost:5000/get_library')
+      .then(response => {
+        setGames(response.data.data)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error)
+      })
+  }
+  console.log(games)
+
 
   return (
     <div>
       <NavBar />
       <div className='library-container-container'>
         <div className='library-container'>
+
+          {games.map(game => {
+            return (
+              <div>
+                <div className='column-game-title' key={game.appid}>{game.name}</div>
+                <hr className="divider" />
+              </div>
+            )
+          })}
         </div>
-        <div className='library-container'>
+        <div className='grid-container'>
+          {games.map((game) => (
+            <LibraryCard key={game.appid} game={game} fetchDataAgain={fetchDataAgain} />
+          ))}
         </div>
       </div>
     </div>
